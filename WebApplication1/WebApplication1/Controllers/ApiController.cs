8b8375fba4d3a4eb5c4e93ec20ea725b7da1dfab
Abhinandan14 <Abhinandan.pun@gmail.com>
2021-06-19 20:02:00 +0530

    JsonConstructs Init
    Intialized models for Json and inlcuded a  (\faq) httpget to demo the progress

---
+++ WebApplication1/WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,45 @@
+﻿using JsonConstructs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonConstructs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : Controller
+    {
+        private static readonly string[] SomeText = new[]
+        {
+            "some", "thing", "cool", "i am", "working", "on", "but", "i have", "little to no", "idea","what","I am","Doing..!!"
+        }; 
+
+        [HttpGet]
+        public string First()
+        {
+            return "DEMO";
+        }
+        
+        [HttpGet("faq")]
+
+        public IEnumerable<FaqModel> GetFaq()
+        {
+            var rng = new Random(); 
+            return Enumerable.Range(1, 5).Select(index => new FaqModel
+            {
+                ID = index,
+                Questions = SomeText[rng.Next(SomeText.Length)],
+                Answers = SomeText[rng.Next(SomeText.Length)],
+                Meta = new MetaModel{
+                    Title = SomeText[rng.Next(SomeText.Length)],
+                    Description = SomeText[rng.Next(SomeText.Length)],
+                }
+
+            })
+            .ToArray();
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/StepsModel.cs
@@ -0,0 +1,17 @@
+﻿using JsonConstructs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonConstructs.Models
+{
+    public class StepsModel
+    {
+        public int Index { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public MetaModel Meta { get; set; }
+    }
+}
